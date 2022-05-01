52df4761d11dc3745da4b30ad0ca1b6162f4bb84
mm1089 <michael@mclain.co.uk>
2022-05-01 15:46:03 +0100

    <added> front end item showcase & footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        bounce: {
+          '0%, 100%': { transform: 'scale(0)'},
+          '50%': { transform: 'scaleY(1)' }
+        }
+        //group-hover:animate-[bounce_2s_ease-in-out_infinite]
+      },
+      colors: {
+        'vue-green': '#41B883',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
